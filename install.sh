43688871e751e767e57a85dc0104d38b9d25ff31
tangjilong <tangjilong@flowam.com>
2016-09-15 19:51:39 +0800

    [pragmatt] - Tuning script.

--- install.sh
+++ install.sh
@@ -47,6 +47,7 @@ ln -sf $PWD/gitconfig $HOME/.gitconfig
 # ========================
 sudo yum install -y zsh
 rm -rf $HOME/.oh-my-zsh
+export ZSH=$HOME/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ln -sf $PWD/zshrc $HOME/.zshrc
 #sudo chsh -s $(which zsh) && exit 0
